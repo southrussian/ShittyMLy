2f4715af70881dde11c3635db9d4f453a7f67860
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2024-01-22 19:30:40 +0300

    load data

--- main.py
+++ main.py
@@ -4,4 +4,12 @@ from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 import spacy
 import plotly.express as px
+from collections import defaultdict
+from collections import Counter
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.decomposition import LatentDirichletAllocation
 
+# nlp = spacy.load('en_core_web_sm')
+
+data = pd.read_csv("articles.csv", encoding='latin-1')
+print(data.head())
