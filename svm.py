a9c476013c3bc4dcd2f4ca3779bfaf133e080d05
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2024-02-14 15:40:48 +0300

    SVM researches: output of clf score

---
+++ svm.py
@@ -0,0 +1,18 @@
+# support vector machine
+
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+
+data = load_breast_cancer()
+
+x = data.data
+y = data.target
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+
+clf = SVC(kernel='linear', C=3)
+clf.fit(x_train, y_train)
+
+print(clf.score(x_test, y_test))
