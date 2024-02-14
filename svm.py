c0b51efb4eb1292573bd3fe0e8d9cc6a51883756
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2024-02-14 16:16:29 +0300

    added two classifiers SVC & KNN: always variable values

--- svm.py
+++ svm.py
@@ -10,9 +10,14 @@ data = load_breast_cancer()
 x = data.data
 y = data.target
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=1)
 
-clf = SVC(kernel='linear', C=3)
-clf.fit(x_train, y_train)
+clf_svc = SVC(kernel='linear', C=3)
+clf_svc.fit(x_train, y_train)
 
-print(clf.score(x_test, y_test))
+clf_knn = KNeighborsClassifier(n_neighbors=3)
+clf_knn.fit(x_train, y_train)
+
+print(clf_svc.score(x_test, y_test))
+print(clf_knn.score(x_test, y_test))
+
